829b7583e9effb675733a849c7efa979b9bcae0b
Alex Murray <murray.alex@gmail.com>
2020-10-02 14:45:58 +0930

    Add LICENSE and relicense under GPL3+
    Fixes issue #25

--- extension.js
+++ extension.js
@@ -2,11 +2,11 @@
 /**
  * Remmina Search Provider for GNOME Shell
  *
- * Copyright (c) 2012 Alex Murray <murray.alex@gmail.com>
+ * Copyright (c) 2020 Alex Murray <murray.alex@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
